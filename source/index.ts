d969d650819950bae375095c4876817c132e03b2
Edgar Toll <edjopato@gmail.com>
2019-11-25 19:25:52 +0100

    feat(castle): add time constants

--- source/index.ts
+++ source/index.ts
@@ -3,5 +3,6 @@ export const BASTION_SIEGE_BOT_ID = 252148344
 export * from './battle'
 export * from './battlereport'
 export * from './buildings'
+export * from './castle'
 export * from './gamescreen'
 export * from './resources'
