059414bae749088c43cfd85e1a947a7ad564329f
Edgar Toll <edjopato@gmail.com>
2019-11-25 19:56:11 +0100

    feat(castle): add castle bonus

--- source/castle.ts
+++ source/castle.ts
@@ -2,3 +2,9 @@ export type Castle = 'diamond' | 'ruby' | 'sapphire'
 
 export const CASTLE_SIEGE_SECONDS = 5 * 60 * 60 // 5 hours
 export const CASTLE_HOLD_SECONDS = 3 * 24 * 60 * 60 // 3 days
+
+export const CASTLE_BONUS: Record<Castle, number> = {
+	diamond: 0.3,
+	ruby: 0.2,
+	sapphire: 0.1
+}
